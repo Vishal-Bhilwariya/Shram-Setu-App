043ca79db74dd91b0e057d8abbbdb06626a130ed
Vishal-Bhilwariya <vishalbhilwariya@gmail.com>
2025-06-23 12:03:55 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:shram_setu_project/pages/auth/auth_page.dart';
 import 'package:shram_setu_project/pages/auth/create_account.dart';
 import 'package:shram_setu_project/pages/auth/login_page.dart';
 import 'package:shram_setu_project/pages/auth/reset_password.dart';
+import 'package:shram_setu_project/pages/auth/success_screen.dart';
 import 'package:shram_setu_project/pages/language_selection.dart';
 import 'package:shram_setu_project/pages/role_selection.dart';
 import 'package:shram_setu_project/pages/welcome_page.dart';
@@ -15,7 +16,14 @@ class ShramSetuApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shram Setu',
-      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFF0F0F0), // default backgroud color
+        primaryColor: Color(0xFFFF6F00),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0xFFFF6F00),
+        ),
+        useMaterial3: true,
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => WelcomePage(),
@@ -25,6 +33,7 @@ class ShramSetuApp extends StatelessWidget {
         '/login': (context) => LoginPage(),
         '/create': (context) => CreateAccountPage(),
         '/reset': (context) => ResetPasswordPage(),
+        '/success': (context) => SuccessScreen(),
       },
     );
   }
